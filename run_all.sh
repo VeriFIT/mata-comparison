ebc8cea6ce3429a413319db312cb954a26d576b2
verifit <verifit@fit.vutbr.cz>
2023-09-18 12:38:04 +0200

    Update experiments

--- run_all.sh
+++ run_all.sh
@@ -2,10 +2,12 @@
 BASEDIR=$(dirname "$0")
 TIMEOUT=60
 JOBS=6
-SUFFIX="--suffix _first-experiments"
+SUFFIX="--suffix .first-real-experiments"
 
 "$BASEDIR/scripts/run_pyco.sh" --config "$BASEDIR/jobs/tacas-24-automata-inclusion.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" $SUFFIX inputs/bench-double-automata-inclusion.input
-"$BASEDIR/scripts/run_pyco.sh" --config "$BASEDIR/jobs/tacas-24-bool-comb-diff.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" $SUFFIX inputs/bench-double-bool-comb-cox.input
+"$BASEDIR/scripts/run_pyco.sh" --config "$BASEDIR/jobs/tacas-24-bool-comb-cox-diff.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" $SUFFIX inputs/bench-double-bool-comb-cox.input
 "$BASEDIR/scripts/run_pyco.sh" --config "$BASEDIR/jobs/tacas-24-bool-comb-cox-inter.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" $SUFFIX inputs/bench-double-bool-comb-cox.input
-"$BASEDIR/scripts/run_pyco.sh" --config "$BASEDIR/jobs/tacas-24-email-filter.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" $SUFFIX inputs/bench-quintuple-email-filter.input
 "$BASEDIR/scripts/run_pyco.sh" --config "$BASEDIR/jobs/tacas-24-bool-comb-intersect.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" $SUFFIX inputs/bench-variadic-bool-comb-intersect.input
+"$BASEDIR/scripts/run_pyco.sh" --config "$BASEDIR/jobs/tacas-24-email-filter.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" $SUFFIX inputs/bench-quintuple-email-filter.input
+"$BASEDIR/scripts/run_pyco.sh" --config "$BASEDIR/jobs/tacas-24-z3-noodler.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" $SUFFIX inputs/bench-single-z3-noodler.input
+"$BASEDIR/scripts/run_pyco.sh" --config "$BASEDIR/jobs/tacas-24-presburger-complement.yaml" --timeout "$TIMEOUT" --jobs $SUFFIX "$JOBS" inputs/bench-single-presburger-complement.input
--- run_test.sh
+++ run_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
-TIMEOUT=2
+TIMEOUT=5
 JOBS=6
 
 "$BASEDIR/scripts/run_pyco.sh" --test-run --config "$BASEDIR/jobs/tacas-24-automata-inclusion.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" inputs/bench-double-automata-inclusion.input
 "$BASEDIR/scripts/run_pyco.sh" --test-run --config "$BASEDIR/jobs/tacas-24-bool-comb-cox-diff.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" inputs/bench-double-bool-comb-cox.input
 "$BASEDIR/scripts/run_pyco.sh" --test-run --config "$BASEDIR/jobs/tacas-24-bool-comb-cox-inter.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" inputs/bench-double-bool-comb-cox.input
-"$BASEDIR/scripts/run_pyco.sh" --test-run --config "$BASEDIR/jobs/tacas-24-email-filter.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" inputs/bench-quintuple-email-filter.input
 "$BASEDIR/scripts/run_pyco.sh" --test-run --config "$BASEDIR/jobs/tacas-24-bool-comb-intersect.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" inputs/bench-variadic-bool-comb-intersect.input
+"$BASEDIR/scripts/run_pyco.sh" --test-run --config "$BASEDIR/jobs/tacas-24-email-filter.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" inputs/bench-quintuple-email-filter.input
+"$BASEDIR/scripts/run_pyco.sh" --test-run --config "$BASEDIR/jobs/tacas-24-z3-noodler.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" inputs/bench-single-z3-noodler.input
+"$BASEDIR/scripts/run_pyco.sh" --test-run --config "$BASEDIR/jobs/tacas-24-presburger-complement.yaml" --timeout "$TIMEOUT" --jobs "$JOBS" inputs/bench-single-presburger-complement.input
